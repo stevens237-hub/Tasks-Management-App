d6d99bddd3d555e5885c457b4432fd896f7391d6
stevens <stevemboda@gmail.com>
2025-12-08 13:38:55 +0100

    Login Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import { useState } from 'react'
 import Login from './pages/Login'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import { Toaster } from 'sonner';
 
+function layout() {
 
+}
 function App() {
-  return <Login />;
+  return (
+    <main className="w-full min-h-screen bg-[#f3f4f6]">
+      <Routes>
+        {/* <Route element = {<layout/>}> */}
+          <Route path="/" element={<Navigate to='/dashboard'/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+        {/* </Route> */}
+
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+
+      <Toaster richColors/>
+    </main>
+  );
 }
 
 export default App
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,8 +1,115 @@
-import React from "react"
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Textbox from "../components/Textbox";
+import Button from "../components/Button";
+
+
 
 const Login = () => {
+  const user = "";
+  const {
+    register, 
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+  
+  const submitHandler = async() => {
+    console.log("Submitted data:");
+  }
+
+  // const handleLogin = async (data) => {
+  //   try {
+
+  //     // Appel de l'API de connexion
+  //     const response = await login(data).unwrap();
+      
+  //     dispatch(setCredentials(response));
+  //     navigate('/');
+  //   } catch (err) {
+  //     toast.error(err?.data?.message || 'Login failed');
+  //   }
+
+  // };
+
+  useEffect(() => {
+    // Si l'utilisateur est déjà connecté, redirige vers le tableau de bord
+    user && navigate('/dashboard');
+    }, [user]);
+
   return (
-    <div>Login Page</div>
+    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6] dark:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-[#302943] via-slate-900 to-black">
+      <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
+        <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
+          <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
+            <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base dark:border-gray-700  border-gray-300 text-gray-600'>
+              Manage all your task in one place!
+            </span>
+            <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
+              <span>EasyTasks</span>
+              <span>Task Manager</span>
+            </p>
+
+            <div className='cell'>
+              <div className='circle rotate-in-up-left'></div>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center'>
+          <form
+            onSubmit={handleSubmit(submitHandler)}
+            className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white  px-10 pt-14 pb-14'
+          >
+            <div>
+              <p className='text-blue-600 text-3xl font-bold text-center'>
+                Welcome back!
+              </p>
+              <p className='text-center text-base text-gray-700 '>
+                Keep all your credetials safe!
+              </p>
+            </div>
+            <div className='flex flex-col gap-y-5'>
+              <Textbox
+                placeholder='you@example.com'
+                type='email'
+                name='email'
+                label='Email Address'
+                className='w-full rounded-full'
+                register={register("email", {
+                  required: "Email Address is required!",
+                })}
+                error={errors.email ? errors.email.message : ""}
+              />
+              <Textbox
+                placeholder='password'
+                type='password'
+                name='password'
+                label='Password'
+                className='w-full rounded-full'
+                register={register("password", {
+                  required: "Password is required!",
+                })}
+                error={errors.password ? errors.password?.message : ""}
+              />
+              <span className='text-sm text-gray-600 hover:underline cursor-pointer'>
+                Forget Password?
+              </span>
+            </div>
+            
+              <Button
+                type='submit'
+                label='Log in'
+                className='w-full h-10 bg-blue-700 text-white rounded-full'
+              />
+            
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }   
 
